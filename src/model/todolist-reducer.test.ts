5f06e27777c098f17ff8fd31b3ffa30bacb6d835
VanoKi <kirsvano@gmail.com>
2025-05-13 19:46:56 +0300

    fix: second test pass

--- src/model/todolist-reducer.test.ts
+++ src/model/todolist-reducer.test.ts
@@ -1,7 +1,31 @@
 import {v1} from 'uuid'
 import {expect, test} from 'vitest'
 import type {Todolist} from "../App.tsx";
-import {DeleteTodolistAC, todolistReducer} from "./todolist-reducer.ts";
+import {createTodolistAC, DeleteTodolistAC, todolistReducer} from "./todolist-reducer.ts";
+
+test('correct todolist should be deleted', () => {
+  const todolistIDd1 = v1()
+  const todolistIDd2 = v1()
+
+  const startState: Todolist[] = [
+    {id: todolistIDd1, title: 'What to learn', filter: 'all'},
+    {id: todolistIDd2, title: 'What to buy', filter: 'all'}
+  ]
+
+  // const action = {
+  //   type: 'delete_todolist',
+  //   payload: {
+  //     id: todolistIDd1
+  //   }
+  // } as const
+
+  const action = DeleteTodolistAC(todolistIDd1)
+
+  const endState = todolistReducer(startState, action)
+
+  expect(endState.length).toBe(1)
+  expect(endState[0].id).toBe(todolistIDd2)
+})
 
 
 test('correct todolist should be created', () => {
@@ -14,7 +38,7 @@ test('correct todolist should be created', () => {
   ]
 
   const title = 'New todolist'
-  const endState = todolistsReducer(startState, createTodolistAC(title))
+  const endState = todolistReducer(startState, createTodolistAC(title))
 
   expect(endState.length).toBe(3)
   expect(endState[2].title).toBe(title)
