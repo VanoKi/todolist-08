97e3dd1ff2e4e9246b99fda1c945342307c1bbc4
VanoKi <kirsvano@gmail.com>
2025-05-13 13:45:57 +0300

    feat: write test

---
+++ src/model/todolist-reducer.test.ts
@@ -0,0 +1,26 @@
+import {v1} from 'uuid'
+import {expect, test} from 'vitest'
+import type {Todolist} from "../App.tsx";
+import type {todolistReducer} from "./todolist-reducer.ts";
+
+test('correct todolist should be deleted', () => {
+  const todolistIDd1 = v1()
+  const todolistIDd2 = v1()
+
+  const startState: Todolist[] = [
+    {id: todolistIDd1, title: 'What to learn', filter: 'all'},
+    {id: todolistIDd2, title: 'What to buy', filter: 'all'}
+  ]
+
+  const action = {
+    type: 'delete_todolist',
+    payload: {
+      id: todolistIDd1
+    }
+  }
+
+  const endState = todolistReducer(startState, action)
+
+  expect(endState.length).toBe(1)
+  expect(endState[0].id).toBe(todolistIDd2)
+})
--- src/model/todolist-reducer.ts
+++ src/model/todolist-reducer.ts
@@ -1,7 +1,18 @@
 import type {Todolist} from "../App.tsx";
 
+type Actions = {
+  type: string
+  payload: any
+}
+
 const initialState: Todolist[]
 
 export const todolistReducer = (state: Todolist[] = initialState, action: any): any => {
-
+  switch (action.type) {
+    case 'delete_todolist' : {
+      return state
+    }
+    default:
+      return state
+  }
 }
