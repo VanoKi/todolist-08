aa72efbde22a7a994c816e0853936e33063787fe
VanoKi <kirsvano@gmail.com>
2025-05-13 13:50:28 +0300

    feat: write function delete todolist

--- src/model/todolist-reducer.ts
+++ src/model/todolist-reducer.ts
@@ -10,7 +10,7 @@ const initialState: Todolist[]
 export const todolistReducer = (state: Todolist[] = initialState, action: any): any => {
   switch (action.type) {
     case 'delete_todolist' : {
-      return state
+      return state.filter(todolist => todolist.id !== action.payload.id)
     }
     default:
       return state
