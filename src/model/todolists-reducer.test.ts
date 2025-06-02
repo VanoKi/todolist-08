b750b085ac182ab29a179868f645d639159d28c9
VanoKi <kirsvano@gmail.com>
2025-06-02 19:43:56 +0300

    feat: second test

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -18,9 +18,30 @@ test('correct todolist should be removed', () => {
     payload: {
       id: todolistId1
     }
-  }
+  } as const
 
   const endState = todolistsReducer(startState, action)
   expect(endState.length).toBe(1)
   expect(endState[0].id).toBe(todolistId2)
+})
+
+test('correct todolist should be added', () => {
+  let todolistId1 = v1()
+  let todolistId2 = v1()
+
+  const startState: Todolist[] = [
+    {id: todolistId1, title: 'What to learn', filter: 'all'},
+    {id: todolistId2, title: 'What to buy', filter: 'all'},
+  ]
+
+  const action = {
+    type: 'ADD-TODOLIST',
+    payload: {
+      title: "New Todolist"
+    }
+  } as const
+
+  const endState = todolistsReducer(startState, action)
+  expect(endState.length).toBe(3)
+  expect(endState[0].id).toBe(action.payload.title)
 })
